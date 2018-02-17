b383d6e62ff00d6420d1a2948efa6bdcd63e4ab5
Jackson <truong.jackson108@gmail.com>
2018-02-17 16:41:23 -0500

    Added Webpack and Babel support for ES6

--- Components/Main.js
+++
@@ -1,8 +0,0 @@
-//Start of Main Class, With ES6 refactor
-var Main = class Welcome extends React.Component {
-  render() {
-    return <h1>Hello Jackson!</h1>;
-  }
-}
-
-ReactDOM.render(React.createElement(Main),document.getElementById('root'));
