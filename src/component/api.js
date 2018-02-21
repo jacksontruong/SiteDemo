c2248cda623b093fc0eb3df8cc75a3e01c66e4bb
Jackson <truong.jackson108@gmail.com>
2018-02-20 23:29:30 -0500

    API prep

---
+++ src/component/api.js
@@ -0,0 +1,7 @@
+//FETCH APIs
+
+//https://www.reddit.com/dev/api/
+//https://developers.google.com/youtube/v3/
+//https://developers.facebook.com/
+//https://developer.twitter.com/
+//https://www.instagram.com/developer/
